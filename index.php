d3eb3827a9b36f302e4294e702351e23dab66c04
Rowan L <hearthgarage@gmail.com>
2021-01-30 20:23:16 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- new attempt at setting up code-->
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<!--from old sheet idk what it does-->
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<!--link to a stylesheet-->
+	<link rel="stylesheet" type="text/css" href="/pages/site_style.css">
+	<title>RL</title>
+</head>
+
+<body>
+<?php include "headernav.php"; ?> 
+<?php
+
+<div class="content">
+<br>
+	<img src="/img/rowan.jpg" style="width:350px">
+
+</div>
+</body>
+</html>
+
+
