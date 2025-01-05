c3199155c4ad5d067731f34402656a9b9adc371d
rowanpersonal <61559422+rowanpersonal@users.noreply.github.com>
2025-01-05 12:40:03 -0500

    Update heavernav.php

--- pages/heavernav.php
+++ pages/heavernav.php
@@ -3,7 +3,6 @@
 	<div>
 		<p> Rowan Lynch
 		<br> @rowanly / <A HREF="/img/CV_RowanLynch.pdf" style="color:black">CV</A>
-		<br> contact: rowanhlynch@gmail.com</p>
 		<p>
 		<A HREF="/pages/curatorial.html" style="color:black">Curatorial</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 		<A HREF="/pages/design.html" style="color:black">Design</A> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
